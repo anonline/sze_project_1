114c2a320a8d6b579b06e4e134adcf9e51743f61
sanyi0411 <sandor.derda@gmail.com>
2020-05-05 16:31:48 +0200

    Create About page

--- angular-sze-project1/src/app/app-routing.module.ts
+++ angular-sze-project1/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainMenuComponent } from './components/main-menu/main-menu.component'
+import { AboutComponent } from './components/pages/about/about.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainMenuComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-sze-project1/src/app/app.module.ts
+++ angular-sze-project1/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MainMenuComponent
+    MainMenuComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
